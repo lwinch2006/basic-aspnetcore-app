0d172569066e1c7a49c85fb7d50c8ac8a7a5b692
D.K <6912915+lwinch2006@users.noreply.github.com>
2021-04-30 10:48:15 +0200

    * Added another variant of function to inject types to IServiceCollection.

--- Dka.AspNetCore.BasicWebApp.Common/src/Logic/ServiceCollection/ServiceCollectionExtensions.cs
+++ Dka.AspNetCore.BasicWebApp.Common/src/Logic/ServiceCollection/ServiceCollectionExtensions.cs
@@ -29,6 +29,38 @@ namespace Dka.AspNetCore.BasicWebApp.Common.Logic.ServiceCollection
                     }
                 }                
             }
+        }
+        
+        public static void InjectTypes(this IServiceCollection services, Type baseInterface, bool injectJustImplementation, params Type[] types)
+        {
+            foreach (var type in types)
+            {
+                var assembly = type.GetTypeInfo().Assembly;
+                
+                var foundTypes = assembly
+                    .GetTypes()
+                    .Select(t1 => new
+                    {
+                        FoundInterface = t1.GetInterfaces().Where(t2 => t2.IsGenericType == baseInterface.IsGenericType).SingleOrDefault(t3 => baseInterface.IsGenericType ? t3.GetGenericTypeDefinition() == baseInterface : t3 == baseInterface), 
+                        FoundClass = t1
+                    })
+                    .Where(t3 => t3.FoundInterface != null);
+
+                if (injectJustImplementation)
+                {
+                    foreach (var foundType in foundTypes)
+                    {
+                        services.AddScoped(foundType.FoundClass);
+                    }
+
+                    return;
+                }
+                
+                foreach (var foundType in foundTypes)
+                {
+                    services.AddScoped(foundType.FoundInterface, foundType.FoundClass);
+                }
+            }
         }        
     }
 }
